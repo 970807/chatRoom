7845754b01d2b70cf59851d359eda3d3319a2cd2
guoguihong <guoguihong@qq.com>
2021-05-06 19:22:26 +0800

    axios增加网络错误提示

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,17 +1,22 @@
 import axios from 'axios'
-
+import Vue from 'vue'
 const service = axios.create({
   baseURL: 'http://localhost:3000/api',
   timeout: 5000,
   withCredentials: false
 })
 
-service.interceptors.response.use(response => {
-  if(response.data.code !== '2000') {
-    return Promise.reject(response.data)
-  } else {
-    return response.data
+service.interceptors.response.use(
+  response => {
+    if(response.data.code !== '2000') {
+      return Promise.reject(response.data)
+    } else {
+      return response.data
+    }
+  },
+  error => {
+    Vue.prototype.$message.error(error.message)
   }
-})
+)
 
 export default service
